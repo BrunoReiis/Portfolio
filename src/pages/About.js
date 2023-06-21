72b4ad242318e5925787d0ece4467bfc5b320040
Bruno Reis <bruno.reiis15@gmail.com>
2023-06-21 18:32:40 -0300

    Atualização Main e inicio da About

--- src/pages/About.js
+++ src/pages/About.js
@@ -2,6 +2,14 @@ import "./About.css";
 
 export const About = () => {
     return (
-        <h1>About</h1>
+        <section>
+            {/* Sou estudante de Ciências da Computação na FMU, programador Fullstack e apaixonado por tecnologia. Estou sempre em busca de novos desafios para 
+            aprimorar minhas habilidades e expandir meus conhecimentos. Com experiência em programação web e habilidades sólidas em front-end e back-end.
+
+            <div className='gif'>
+                <img src='https://animesher.com/orig/2/207/2071/20716/animesher.com_animated-animation-laptop-2071674.gif' alt='gif'></img>
+            </div> */}
+
+        </section>
     );
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,25 +4,33 @@ export const Home = () => {
     return (
 
         <section id='main' className='main'>
-            <h1>Olá, sou Bruno Reis!</h1>
-            <h2>Júnior Fullstack Developer</h2>
-            <h2>Linguagens:</h2>
-            <div className='iconsLinguagens'>
-                <a href="https://react.dev" target='_blank' rel="noreferrer"><img align="center" alt="React" height="30" width="40" src="https://raw.githubusercontent.com/devicons/devicon/master/icons/react/react-original.svg"></img></a>
-                <a href="https://developer.mozilla.org/pt-BR/docs/Web/JavaScript" target='_blank' rel="noreferrer"><img align="center" alt="Js" height="30" width="40" src="https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-plain.svg"></img></a>
-                <a href="https://www.typescriptlang.org" target='_blank' rel="noreferrer"><img align="center" alt="TypeJs" height="30" width="40" src="https://raw.githubusercontent.com/devicons/devicon/master/icons/typescript/typescript-plain.svg"></img></a>
-                <a href="https://developer.mozilla.org/pt-BR/docs/Web/HTML" target='_blank' rel="noreferrer"><img align="center" alt="HTML" height="30" width="40" src="https://raw.githubusercontent.com/devicons/devicon/master/icons/html5/html5-original.svg"></img></a>
-                <a href="https://developer.mozilla.org/pt-BR/docs/Web/CSS" target='_blank' rel="noreferrer"><img align="center" alt="CSS" height="30" width="40" src="https://raw.githubusercontent.com/devicons/devicon/master/icons/css3/css3-original.svg"></img></a>
-                <a href="https://nodejs.org/en" target='_blank' rel="noreferrer"><img align="center" alt="nodejs" height="30" width="40" src="https://cdn.worldvectorlogo.com/logos/nodejs-icon.svg"></img></a>
-                <a href="https://git-scm.com" target='_blank' rel="noreferrer"><img align="center" alt="git" height="30" width="40" src="https://raw.githubusercontent.com/devicons/devicon/master/icons/git/git-original.svg"></img></a>
-                <a href="https://www.python.org" target='_blank' rel="noreferrer"><img align="center" alt="Python" height="30" width="40" src="https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg"></img></a>
-                <a href="https://angular.io" target='_blank' rel="noreferrer"><img align="center" alt="Angular" height="30" width="40" src="https://raw.githubusercontent.com/devicons/devicon/master/icons/angularjs/angularjs-original.svg"></img></a>
-                <a href="https://learn.microsoft.com/pt-br/dotnet/csharp/" target='_blank' rel="noreferrer"><img align="center" alt="C#" height="30" width="40" src="https://raw.githubusercontent.com/devicons/devicon/master/icons/csharp/csharp-original.svg"></img></a>
+            <div className='infoMain'>
+                <h1>Olá, sou Bruno Reis!</h1>
+                <h2>Programador FullStack Júnior</h2>
+                <h2>Habilidades:</h2>
+                <div className='iconsLinguagens'>
+                    <a href="https://react.dev" target='_blank' rel="noreferrer"><img align="center" alt="React" height="30" width="40" src="https://raw.githubusercontent.com/devicons/devicon/master/icons/react/react-original.svg"></img></a>
+                    <a href="https://developer.mozilla.org/pt-BR/docs/Web/JavaScript" target='_blank' rel="noreferrer"><img align="center" alt="Js" height="30" width="40" src="https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-plain.svg"></img></a>
+                    <a href="https://www.typescriptlang.org" target='_blank' rel="noreferrer"><img align="center" alt="TypeJs" height="30" width="40" src="https://raw.githubusercontent.com/devicons/devicon/master/icons/typescript/typescript-plain.svg"></img></a>
+                    <a href="https://developer.mozilla.org/pt-BR/docs/Web/HTML" target='_blank' rel="noreferrer"><img align="center" alt="HTML" height="30" width="40" src="https://raw.githubusercontent.com/devicons/devicon/master/icons/html5/html5-original.svg"></img></a>
+                    <a href="https://developer.mozilla.org/pt-BR/docs/Web/CSS" target='_blank' rel="noreferrer"><img align="center" alt="CSS" height="30" width="40" src="https://raw.githubusercontent.com/devicons/devicon/master/icons/css3/css3-original.svg"></img></a>
+                    <a href="https://nodejs.org/en" target='_blank' rel="noreferrer"><img align="center" alt="nodejs" height="30" width="40" src="https://cdn.worldvectorlogo.com/logos/nodejs-icon.svg"></img></a>
+                    <a href="https://git-scm.com" target='_blank' rel="noreferrer"><img align="center" alt="git" height="30" width="40" src="https://raw.githubusercontent.com/devicons/devicon/master/icons/git/git-original.svg"></img></a>
+                    <a href="https://www.python.org" target='_blank' rel="noreferrer"><img align="center" alt="Python" height="30" width="40" src="https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg"></img></a>
+                    <a href="https://angular.io" target='_blank' rel="noreferrer"><img align="center" alt="Angular" height="30" width="40" src="https://raw.githubusercontent.com/devicons/devicon/master/icons/angularjs/angularjs-original.svg"></img></a>
+                    <a href="https://learn.microsoft.com/pt-br/dotnet/csharp/" target='_blank' rel="noreferrer"><img align="center" alt="C#" height="30" width="40" src="https://raw.githubusercontent.com/devicons/devicon/master/icons/csharp/csharp-original.svg"></img></a>
+                </div>
+
+                <div className='buttons'>
+                    <button>About Me</button>
+                    <button>View CV</button>
+                </div>
             </div>
 
-            <div className='buttons'>
-                <button>About Me</button>
-                <button>View CV</button>
+            <div className="gifAtomo">
+                <div className="circulo Um"></div>
+                <div className="circulo Dois"></div>
+                <div className="circulo Tres"></div>
             </div>
         </section>
     );
