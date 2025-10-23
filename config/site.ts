a699b44de2cb815442ef7a325914b7cdc8ba8dbe
Bruno Reis <bruno.reiis15@gmail.com>
2025-10-22 22:53:46 -0300

    init: cleaning

--- config/site.ts
+++ config/site.ts
@@ -5,58 +5,38 @@ export const siteConfig = {
   description: "Make beautiful websites regardless of your design experience.",
   navItems: [
     {
-      label: "Home",
+      label: "Inicio",
       href: "/",
     },
     {
-      label: "Docs",
+      label: "Sobre",
       href: "/docs",
     },
     {
-      label: "Pricing",
+      label: "Habilidades",
       href: "/pricing",
     },
     {
-      label: "Blog",
+      label: "Projetos",
       href: "/blog",
-    },
-    {
-      label: "About",
-      href: "/about",
     },
   ],
   navMenuItems: [
-    {
-      label: "Profile",
-      href: "/profile",
+        {
+      label: "Inicio",
+      href: "/",
     },
     {
-      label: "Dashboard",
-      href: "/dashboard",
+      label: "Sobre",
+      href: "/docs",
     },
     {
-      label: "Projects",
-      href: "/projects",
+      label: "Habilidades",
+      href: "/pricing",
     },
     {
-      label: "Team",
-      href: "/team",
-    },
-    {
-      label: "Calendar",
-      href: "/calendar",
-    },
-    {
-      label: "Settings",
-      href: "/settings",
-    },
-    {
-      label: "Help & Feedback",
-      href: "/help-feedback",
-    },
-    {
-      label: "Logout",
-      href: "/logout",
+      label: "Projetos",
+      href: "/blog",
     },
   ],
   links: {
